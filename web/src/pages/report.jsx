fda97ccc13ef211d1b7befbbab159fd1a008383e
rayferric <63957587+rayferric@users.noreply.github.com>
2024-09-29 11:45:08 +0200

    display results

--- web/src/pages/report.jsx
+++ web/src/pages/report.jsx
@@ -1,24 +1,19 @@
 import styles from "./report.module.css";
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 import React from "react";
 import Plot from "react-plotly.js";
 
 let reportVideo = null;
-let job_id = null;
-let intervalId = null;
+let reportData = null;
 
 function setReportVideo(video) {
   reportVideo = video;
   window.dispatchEvent(new Event("setReportVideo"));
 }
 
-function setJobId(id) {
-  job_id = id;
-  window.dispatchEvent(new Event("setJobId"));
-}
-
-function setIntervalId(id) {
-  intervalId = id;
+function setReportData(data) {
+  reportData = data;
+  window.dispatchEvent(new Event("setReportData"));
 }
 
 function StyledPlot({ className, title, data }) {
@@ -47,132 +42,141 @@ function StyledPlot({ className, title, data }) {
 
 export default function Report() {
   // const [intervalId, setIntervalId] = React.useState(null);
+  const [_, setRerenderCount] = React.useState(0);
 
-  const handleData = (data) => {
-    console.log("Data received: ", data);
-    clearInterval(intervalId);
-  };
-
-  const sendJobRequest = (job_id) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "/get_job/" + job_id, true);
-    xhr.onload = () => {
-      if (xhr.status === 200) {
-        const response = JSON.parse(xhr.responseText);
-        console.log("Job data received successfully");
-        handleData(response);
-      } else {
-        let err = xhr.statusText;
-        if (xhr.status == 400) {
-          const text = xhr.responseText;
-          err = JSON.parse(text).error;
-          alert("Nie udało się wysłać zapytania: " + err);
-        }
-      }
-    };
-    xhr.onerror = () => {
-      console.error("An error occurred during the job request");
-    };
-    xhr.send(JSON.stringify({ job_id: job_id }));
-  };
+  const rerender = useCallback(() => {
+    setRerenderCount((prev) => prev + 1);
+  }, []);
 
   useEffect(() => {
-    const listener = () => {
-      console.log("reportVideo:", reportVideo);
+    const setReportVideoListener = () => {
+      rerender();
     };
-      
-    const intervalId = setInterval(() => {
-        sendJobRequest(job_id);
-    }, 1000);
-    setIntervalId(intervalId);
-    
-    window.addEventListener("setReportVideo", listener);
-    window.addEventListener("setJobId", listener);
+
+    const setReportDataListener = () => {
+      rerender();
+    };
+
+    window.addEventListener("setReportVideo", setReportVideoListener);
+    window.addEventListener("setReportData", setReportDataListener);
     return () => {
-      window.removeEventListener("setReportVideo", listener);
-      window.removeEventListener("setJobId", listener);
+      window.removeEventListener("setReportVideo", setReportVideoListener);
+      window.removeEventListener("setReportData", setReportDataListener);
     };
   }, []);
 
-  let transcript =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+  if (!reportData) {
+    return <div>Waiting for data...</div>;
+  }
 
-  let audio_errors = 0;
+  // Compute number of words in each second to create a kind of bucket plot
+  const wordsPerSecond = {};
+  let currentSecond = 1;
+  for (let i = 0; i < reportData.word_timestamps.length; i++) {
+    const word = reportData.word_timestamps[i];
+    if (word.start < currentSecond) {
+      if (wordsPerSecond[currentSecond] === undefined) {
+        wordsPerSecond[currentSecond] = 1;
+      } else {
+        wordsPerSecond[currentSecond]++;
+      }
+    }
+    else {
+      currentSecond++;
+      i--;
+    }
+  }
+  const wordsPerSecondX = Object.keys(wordsPerSecond);
+  const wordsPerSecondY = Object.values(wordsPerSecond);
 
   return (
     <div className={styles["report"]}>
       <div className={styles["video-pane"]}>
         <div className={styles["slogan"]}>Wyniki analizy</div>
         <video controls className={styles["video"]}>
-          <source
-                        src={URL.createObjectURL(reportVideo)}
-                        type="video/mp4"
-                    />
+          {reportVideo && (
+            <source
+              src={URL.createObjectURL(reportVideo)}
+              type="video/mp4"
+            />
+          )}
           {/* <source
             src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
             type="video/mp4"
           /> */}
           Your browser does not support the video tag.
         </video>
-        <div className={styles["transcript"]}> Transkrypcja </div>
+      </div>
+      <div className={styles["results-pane"]}>
+        <div className={styles["result-title"]}>Transkrypcja</div>
         <div className={styles["transcript-pane"]}>
-          {transcript.split("\n").map((line, index) => (
+          {reportData.sp.split("\n").map((line, index) => (
             <div key={index}>{line}</div>
           ))}
         </div>
-      </div>
-      <div className={styles["results-pane"]}>
         <div className={styles["result-title"]}>Podsumowanie</div>
         <div className={styles["result-cards"]}>
           <div className={styles["result"]}>
-            <div className={styles["result-label"]}>100</div>
+            <div className={styles["result-label"]}>{reportData.sp.split(" ").length}</div>
             <div className={styles["result-value"]}>Liczba słów</div>
           </div>
           <div className={styles["result"]}>
-            <div className={styles["result-label"]}>100</div>
-            <div className={styles["result-value"]}>Liczba słów</div>
+            <div className={styles["result-label"]}>{parseFloat(reportData.gunning_fog.score).toFixed(2)}</div>
+            <div className={styles["result-value"]}>Współczynnik Mglistości Gunninga</div>
+          </div>
+          <div className={styles["result"]}>
+            <div className={styles["result-label"]}>{reportData.comparison.description.replace(/'/g, "")}</div>
+            <div className={styles["result-value"]}>Poprawność Napisów</div>
+          </div>
+          <div className={styles["result"]}>
+            <div className={styles["result-label"]}>{reportData.background_person_detection.length}</div>
+            <div className={styles["result-value"]}>Liczba sylwetek w tle</div>
+          </div>
+          <div className={styles["result"]}>
+            <div className={styles["result-label"]}>{reportData.head_movement.head_movement[0]}</div>
+            <div className={styles["result-value"]}>Częstość ruchów głowy</div>
           </div>
         </div>
-        <div className={styles["result-title"]}>Wykryte błędy</div>
+        <div className={styles["result-title"]}>Analiza Audio</div>
         <div className={styles["result-cards"]}>
           <div className={styles["result"]}>
-            <div className={styles["result-label"]}>Dźwiękowe</div>
-            <div
-              className={
-                styles["result-value"] + " " + (audio_errors == 0)
-                  ? styles["great"]
-                  : audio_errors < 3
-                    ? styles["ok"]
-                    : styles["bad"]
-              }
-            >
-              Przerywniki: {audio_errors}
-            </div>
+            <div className={styles["result-label"]}>{reportData.audio_analysis.loud_moments.length}</div>
+            <div className={styles["result-value"]}>Głośne momenty</div>
           </div>
           <div className={styles["result"]}>
-            <div className={styles["result-label"]}>Treść</div>
-            <div className={styles["result-value"]}>Liczba słów</div>
+            <div className={styles["result-label"]}>{reportData.audio_analysis.long_silence.length}</div>
+            <div className={styles["result-value"]}>Długie cisze</div>
           </div>
           <div className={styles["result"]}>
-            <div className={styles["result-label"]}>Wizualne</div>
-            <div className={styles["result-value"]}>Liczba słów</div>
+            <div className={styles["result-label"]}>{reportData.audio_analysis.repeating_sound.length}</div>
+            <div className={styles["result-value"]}>Powtarzające się dźwięki</div>
           </div>
         </div>
+        <div className={styles["result-title"]}>Wykryte emocje</div>
+        <div className={styles["result-cards"]}>
+          <ul>
+            {Object.entries(reportData.emotions_detection.emotions_detection.emotions).map(([stamp, emotion]) => (
+              <li key={emotion}>{stamp}: {emotion}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className={styles["result-title"]}>Statystyki Czasowe</div>
         <div className={styles["plot-container"]}>
           <StyledPlot
             className={styles["plot"]}
-            title="Jakość Przekazu"
+            title="Słowa na Sekundę"
             data={[
               {
-                x: [1, 2, 3, 7],
-                y: [2, 6, 3, 1],
+                x: wordsPerSecondX,
+                y: wordsPerSecondY,
                 type: "scatter",
                 mode: "lines+markers",
                 marker: { color: "#e6a" },
               },
             ]}
           />
-          <StyledPlot
+          {/* <StyledPlot
             className={styles["plot"]}
             title="Jakość Przekazu"
             data={[
@@ -197,11 +201,11 @@ export default function Report() {
                 marker: { color: "#e86" },
               },
             ]}
-          />
+          /> */}
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 
-export { setReportVideo, setJobId };
+export { setReportVideo, setReportData };
