277765682712a595ba0893df62d4876546c32618
Kacper Iwicki <kacper.iwi@gmail.com>
2024-09-29 08:42:41 +0200

    updates to report

--- web/src/pages/report.jsx
+++ web/src/pages/report.jsx
@@ -1,5 +1,6 @@
 import styles from "./report.module.css";
 import { useEffect } from "react";
+import Plot from "react-plotly.js";
 
 let reportVideo = null;
 
@@ -19,28 +20,41 @@ export default function Report() {
         };
     }, []);
 
+    let transcript =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
     return (
         <div className={styles["report"]}>
             <div className={styles["video-pane"]}>
                 <div className={styles["slogan"]}>Wyniki analizy</div>
                 <video controls className={styles["video"]}>
-                    <source
+                    {/* <source
                         src={URL.createObjectURL(reportVideo)}
                         type="video/mp4"
+                    /> */}
+                    <source
+                        src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+                        type="video/mp4"
                     />
-                    {/* <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4" /> */}
                     Your browser does not support the video tag.
                 </video>
+                <div className={styles["transcript"]}> Transkrypcja </div>
+                <div className={styles["transcript-pane"]}>
+                    {transcript.split("\n").map((line, index) => (
+                        <div key={index}>{line}</div>
+                    ))}
+                </div>
             </div>
             <div className={styles["results-pane"]}>
-                Lorem ipsum dolor sit amet
-                <br />
-                consectetur adipiscing elit
-                <br />
-                sed do eiusmod tempor incididunt
-                <br />
-                ut labore et dolore magna aliqua
-                <br />
+                <div className={styles["result-title"]}>Podsumowanie</div>
+                <div className={styles["result-cards"]}>
+                    <div className={styles["result"]}>
+                        <div className={styles["result-label"]}>
+                            Liczba słów
+                        </div>
+                        <div className={styles["result-value"]}>100</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
